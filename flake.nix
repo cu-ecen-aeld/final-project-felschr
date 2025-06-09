72abaf6a9dc91531dc8cf4dfa694efb7692d6bf5
Felix Schröter <dev@felschr.com>
2025-06-09 21:56:45 +0200

    build: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,64 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs =
+    inputs:
+    inputs.flake-utils.lib.eachSystem [ "x86_64-linux" ] (
+      system:
+      let
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+        inherit (pkgs) lib;
+
+        fhs-build = pkgs.buildFHSEnv {
+          name = "buildroot-env";
+          targetPkgs =
+            pkgs:
+            (
+              with pkgs;
+              [
+                (lib.hiPrio gcc)
+                binutils
+                cpio
+                elfutils.dev
+                file
+                gnumake
+                ncurses.dev
+                pkg-config
+                unzip
+                wget
+                which
+                pkgsCross.aarch64-multiplatform.gccStdenv.cc
+
+                # crosstool-NG
+                autoconf
+                automake
+                libtool
+                gperf
+                bison
+                flex
+                texinfo
+                help2man
+
+                python3
+              ]
+              ++ pkgs.linux.nativeBuildInputs
+            );
+          runScript = pkgs.writeShellScript "fhs-build" ''
+            echo "Building..."
+            ./rebuild.sh
+            echo "Build finished"
+          '';
+        };
+      in
+      {
+        devShells = {
+          fhs-build = fhs-build.env;
+        };
+      }
+    );
+}
